20b52efbc7361dd84de137c47a62c431a18a2bde
박세희 <164884503+sehuiiii@users.noreply.github.com>
2024-06-16 17:18:03 +0900

    Delete Baekjoon.5622.py

--- Baekjoon.5622.py
+++
@@ -1,8 +0,0 @@
-dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
-a = input()
-r = 0
-for j in range(len(a)):
-    for i in dial:
-        if a[j] in i:
-            r += dial.index(i)+3
-print(r)
